e013d346857c3faf86ae8187bda9e65fce764165
Johannes Dierkes <johannesdierkes@t-online.de>
2017-05-02 10:21:13 +0200

    actions and outlets set for: pictureView, textfields, imapickercontroller. Next up: Subscribe to Keyboardnotifications to get keyboard hight

--- MemeMeV1/ViewController.swift
+++ MemeMeV1/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UITextFieldDelegate, UINavigationControllerDelegate {
 
     
     @IBOutlet weak var topToolBar: UIToolbar!
@@ -22,13 +23,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextfield: UITextField!
     
+    let memeTextAttributes:[String:Any] = [NSStrokeColorAttributeName: UIColor.black, NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!, NSStrokeWidthAttributeName: "3.0"]
     
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        topTextField.delegate = self
+        bottomTextfield.delegate = self
+        
+        topTextField.text = "TOP"
+        bottomTextfield.text = "BOTTOM"
+        
+        let textfields = [topTextField, bottomTextfield]
+        
+        for textField in textfields {
+            textField?.defaultTextAttributes = memeTextAttributes
+            textField?.textAlignment = .center
+        }
+        
+    
+        
     }
 
+    // disable camera-Button, as no camera is available in Simulator
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        cameraButtonOutlet.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
     
     @IBAction func shareButton(_ sender: Any) {
     }
@@ -37,9 +60,40 @@ class ViewController: UIViewController {
     }
 
     @IBAction func cameraButton(_ sender: Any) {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .camera
+        
+        self.present(cameraPicker, animated: true, completion: nil)
     }
     
     @IBAction func albumButton(_ sender: Any) {
+        let albumPicker = UIImagePickerController()
+        albumPicker.delegate = self
+        albumPicker.sourceType = .photoLibrary
+        
+        self.present(albumPicker, animated: true, completion: nil)
+    }
+    
+    // assign selected image to PictureView
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            pictureView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // editing of textfields
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" || textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    // hide keyboard if return button pressed in one of the textfields
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
 }
